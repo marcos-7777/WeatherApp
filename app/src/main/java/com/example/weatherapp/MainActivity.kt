2991cdb550ea694089f21dbf6dd05f04416574ed
MarcosAraujo <marcos.arq@protonmail.com>
2023-11-02 20:41:38 +0000

    2023-11-02 NomeDataLocal ImagemCentral

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.weatherapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -74,14 +77,68 @@ fun GreetingPreview() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 380, heightDp = 800)
+@Composable
+fun page(): Unit {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(12.dp)) {
+        Cabecalho()
+        NomeDataLocal("Marcos")
+        ImagemCentral()
+
+    }
+
+}
+
+@Preview(showBackground = true, widthDp = 380, heightDp = 350)
+@Composable
+fun ImagemCentral(): Unit {
+    Box (modifier = Modifier
+        .height(300.dp)
+        .background(Color.Cyan)
+        .fillMaxWidth()
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.storm),
+                    contentDescription = "Storm")
+            }
+            Row (verticalAlignment = Alignment.Bottom,
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center){
+                Text(text = "16ºC",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(55f, TextUnitType.Sp),
+                    color = MaterialTheme.colorScheme.secondary)
+
+                Text(text = "Tempestade",
+                    fontWeight = FontWeight.Light,
+                    fontSize = TextUnit(15f, TextUnitType.Sp),
+                    color = MaterialTheme.colorScheme.secondary)
+
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true, widthDp = 380, heightDp = 50)
 @Composable
 fun Cabecalho(): Unit {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(12.dp, 8.dp)
+        modifier = Modifier
+            .padding(12.dp, 8.dp)
+            .fillMaxWidth()
     ) {
+
         Button(
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
@@ -99,7 +156,7 @@ fun Cabecalho(): Unit {
             contentPadding = PaddingValues(),
             modifier = Modifier
                 .clip(CircleShape)
-                .width(35.dp)
+                .size(35.dp)
             ) {
 
                 Image(
@@ -145,15 +202,7 @@ fun NomeDataLocal(name: String = "Marcos"): Unit {
     }
 }
 
-@Preview(showBackground = true, widthDp = 380)
-@Composable
-fun Imagem(): Unit {
-    Image(
-        painter = painterResource(id = R.drawable.cloudy),
-        contentDescription = "Cloudy"
-    )
-    
-}
+
 fun formatData(data: Date): String {
     val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
     return format.format(data)
