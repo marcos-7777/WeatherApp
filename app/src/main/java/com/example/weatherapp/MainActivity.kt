d62de960af6af4a46763b872781ff00a51d0a665
MarcosAraujo <marcos.arq@protonmail.com>
2023-11-02 19:48:58 +0000

    2023-11-02 NomeDataLocal

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -25,10 +28,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,7 +97,9 @@ fun Cabecalho(): Unit {
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
             contentPadding = PaddingValues(),
-            modifier = Modifier.clip(CircleShape).width(35.dp)
+            modifier = Modifier
+                .clip(CircleShape)
+                .width(35.dp)
             ) {
 
                 Image(
@@ -98,3 +111,50 @@ fun Cabecalho(): Unit {
         }
 
     }
+
+
+@Preview(showBackground = true, widthDp = 380)
+@Composable
+fun NomeDataLocal(name: String = "Marcos"): Unit {
+    Column (modifier = Modifier.fillMaxWidth()){
+        Row (modifier = Modifier.fillMaxWidth())
+        {
+            Text(text = "Olá",
+                modifier = Modifier.padding(end = 2.dp),
+                fontSize = TextUnit(25f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.primary
+                )
+
+            Text(text = "$name",
+                fontWeight = FontWeight.Bold,
+                fontSize = TextUnit(25f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+        Text(text = formatData(Calendar.getInstance().time),
+            fontWeight = FontWeight.Light,
+            fontSize = TextUnit(17f, TextUnitType.Sp),
+            color = MaterialTheme.colorScheme.secondary
+        )
+
+        Text(text = "Vila Franca de Xira",
+            fontWeight = FontWeight.Light,
+            fontSize = TextUnit(17f, TextUnitType.Sp),
+            color = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 380)
+@Composable
+fun Imagem(): Unit {
+    Image(
+        painter = painterResource(id = R.drawable.cloudy),
+        contentDescription = "Cloudy"
+    )
+    
+}
+fun formatData(data: Date): String {
+    val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
+    return format.format(data)
+}
